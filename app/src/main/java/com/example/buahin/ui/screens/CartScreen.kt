a877436db14b3e280aabd5dc0e9070a938b37920
Simi Prambos <simi.prambos@gmail.com>
2021-12-21 03:52:14 +0700

    feat: add checkout button in cart screen

--- app/src/main/java/com/example/buahin/ui/screens/CartScreen.kt
+++ app/src/main/java/com/example/buahin/ui/screens/CartScreen.kt
@@ -1,7 +1,9 @@
 package com.example.buahin.ui.screens
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.FabPosition
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,18 +12,33 @@ import androidx.compose.ui.unit.dp
 import com.example.buahin.ui.components.CartCard
 import com.example.buahin.ui.components.TopBar
 import com.example.buahin.ui.components.ItemDivider
+import com.example.buahin.ui.components.RoundedButton
 import com.example.buahin.ui.theme.BuahinTheme
 
 @Composable
 fun CartScreen() {
-    Scaffold(topBar = { TopBar(title = "My Cart") }) {
-        LazyColumn(modifier = Modifier.padding(horizontal = 16.dp)) {
-            repeat(5) {
-                item {
-                    CartCard()
-                    ItemDivider(0.dp)
-                }
+    Scaffold(
+        topBar = { TopBar(title = "My Cart") },
+        floatingActionButton = {
+            RoundedButton.Filled(
+                "Go to Checkout",
+                onClick = {},
+                modifier = Modifier.padding(bottom = 60.dp, start = 16.dp, end = 16.dp),
+            )
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            repeat(10) {
+                CartCard()
+                ItemDivider(0.dp)
             }
+            Spacer(modifier = Modifier.height(200.dp))
         }
     }
 }
